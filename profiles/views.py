f5e057d8801e9da850191db75c994f004df8a119
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-28 15:31:24 +0000

    Fix errors in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,32 +1,34 @@
+from p5_api.permissions import IsOwnerOrReadOnly
 from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-from p5_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles,
-        many=True, context={'request': request})
+        serializer = ProfileSerializer(
+            profiles,
+            many=True,
+            context={'request': request})
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
 
     def get_object(self, pk):
         try:
-            profile =Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
             self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
-    
+
     def get(self, request, pk):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile, context={'request': request})
@@ -34,8 +36,10 @@ class ProfileDetail(APIView):
     
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile,
-        data=request.data, context={'request': request})
+        serializer = ProfileSerializer(
+            profile,
+            data=request.data,
+            context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
