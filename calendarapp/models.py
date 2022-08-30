09fdf912307bbc894678ee89df098f48311c85c0
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-30 15:50:16 +0000

    Add model-instructions to calendar

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -4,5 +4,18 @@ import calendar
 from calendar import HTMLCalendar
 
 
-class Calendar(HTMLCalendar):
-
+class Calendar(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    time_field = models.TimeField(blank=True)
+    date_field = models.DateField(blank=True)
+    month_field = models.DateTimeField()
+    header = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    file = models.FileField(
+        max_length=None,
+        allow_empty_file=False,
+        use_url=UPLOADED_FILES_USE_URL)
+    alert = models.DurationField(max_value=None, min_value=None)
