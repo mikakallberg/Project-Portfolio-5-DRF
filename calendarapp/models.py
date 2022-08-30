bbace55d9ece7c171f9e30733cf418d998b7b772
mikakallberg <mikaela.kallberg@hotmail.com>
2022-08-30 15:21:45 +0000

    install calendar-plus, added to requirements.txt

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+import calendar
+from calendar import HTMLCalendar
 
-# Create your models here.
+
+class Calendar(HTMLCalendar):
+
