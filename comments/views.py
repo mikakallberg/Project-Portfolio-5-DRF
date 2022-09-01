5859c7f49bc5ffdb49bb78ff0ba00353b8dc1a6d
mikakallberg <mikaela.kallberg@hotmail.com>
2022-09-01 15:04:31 +0000

    Add comment-functionality. create, edit and delete

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+""" Comments view"""
+from rest_framework import generics, permissions
+from p5_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments
+    Create comment for authenticated users
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    For authenticated users
+    get, edit and delete own comments
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
