6a492d71a1ff0aab6dc042b6de5db06934c4cb65
mikakallberg <mikaela.kallberg@hotmail.com>
2022-09-06 15:51:37 +0000

    Add filtering to profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 """
 Profile view instructions
 """
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from p5_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -11,8 +13,29 @@ class ProfileList(generics.ListAPIView):
     """
     Listing all User profiles
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count(
+            'owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -20,6 +43,9 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     View of individual user profile
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
