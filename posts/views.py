03497ef4e2deafadb1e96c8784835c1abda2d2ad
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-26 12:13:49 +0000

    Change order of imports on post/views.py

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,10 @@ Posts view
 """
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
-from .models import Post
-from .serializers import PostSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from p5_api.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
