f8c7f9220722b189403b402cdc6f71981928a75f
mikakallberg <mikaela.kallberg@hotmail.com>
2022-10-26 12:19:02 +0000

    Remove commented out message

--- followers/views.py
+++ followers/views.py
@@ -26,5 +26,3 @@ class FollowerDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
-
-# Add restrictive functions for User?
